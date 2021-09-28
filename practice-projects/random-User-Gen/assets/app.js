2a91033e438bdabad0d690e557adaa013c9046c3
r-e-d-ant <mugishathierry65@gmail.com>
2021-09-28 22:52:18 +0200

    commented the codes

--- practice-projects/random-User-Gen/assets/app.js
+++ practice-projects/random-User-Gen/assets/app.js
@@ -1,4 +1,5 @@
 
+// app instance
 const app = Vue.createApp({
     data() {
         return {
@@ -10,15 +11,20 @@ const app = Vue.createApp({
         }
     },
     methods: {
+        // function to call random user api then fetch their datas
         getUser() {
             const getUser = async () => {
+                // wait for response
                 const response = await fetch("https://randomuser.me/api/");
+                // throw an error if response status doesn't equal to 200(ok)
                 if (response.status !== 200){
                     throw new Error("Cannot fetch the data");
                 }
+                // store fetched data in data variable then return them
                 const data = await response.json();
                 return data;
             }
+            // call get user function then populate all the variables with data gotten
             getUser().then( data => {
                 this.firstName = data.results[0].name.first,
                 this.lastName = data.results[0].name.last,
@@ -26,9 +32,10 @@ const app = Vue.createApp({
                 this.gender = data.results[0].gender,
                 this.picture = data.results[0].picture.large
             }
-            ).catch(err => console.log("rejected", err.message))
+            ).catch(err => console.log("rejected", err.message)) // Show an error message if there is any
         }
     }
 })
 
+// mounting the app
 app.mount('#app');
