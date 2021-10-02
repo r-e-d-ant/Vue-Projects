c06570dda3d314cd46ebcbc4b3abf992037003fd
r-e-d-ant <mugishathierry65@gmail.com>
2021-10-02 09:47:52 +0200

    fixed layout

---
+++ practice-projects/vue-crash-2021/src/components/Tasks.vue
@@ -0,0 +1,23 @@
+
+<template>
+<div>
+    <div  v-for="task in tasks" :key="task.id">
+        <Task :task="task"/>
+    </div>
+</div>
+</template>
+
+
+<script>
+import Task from  './Task'
+
+export default {
+    name: "Tasks",
+    props: {
+        tasks: Array
+    },
+    components: {
+        Task
+    }
+}
+</script>
